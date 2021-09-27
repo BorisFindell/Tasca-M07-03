a16f46b24265378c8ab772722723d94f075eba87
Martín A. Frangul <postguerra@gmail.com>
2021-09-27 20:17:10 +0200

    Intentando validar3

--- javascript.js
+++ javascript.js
@@ -11,37 +11,23 @@
 
 
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-
-
 let nombre;
 let correu;
 
 document.getElementById('enviar').addEventListener('click', function(){
   enviar()
 })
+
+document.getElementById('form').checkValidity()
+
 function enviar() {
+      
       nombre = document.getElementById('nom').value
       console.log(nombre);
       correu = document.getElementById('email').value
       console.log(correu);
-}
+      pass = document.getElementById('pass').value
+      console.log(pass);
+      prov = document.getElementById('prov').value
+      console.log(prov);
+}     
